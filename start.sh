7c76092bc9ed1e073f60abaf1a1fd3e0a0a90044
Johan Lundberg <lundberg@nordu.net>
2016-04-21 14:25:15 +0200

    Add etcd to hosts file for bootstrapping

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ fi
 # Set up entrys in /etc/hosts for the containers with externally accessible services
 #
 (printf "172.16.20.100\tse-leg-op.seleg_dev se-leg-op\n";
+printf "172.16.20.254\tetcd.seleg_dev etcd\n";
 ) \
     | while read line; do
     if ! grep -q "^${line}$" /etc/hosts; then
