9cc5f44e308a252c033f8066ea009e908a483d98
Fredrik Thulin <fredrik@thulin.net>
2016-04-21 13:42:28 +0200

    set up se-leg-op.seleg_dev in /etc/hosts

--- start.sh
+++ start.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 
-if [ ! -x ./bin/docker-compose ]; then
+if [ ! -f eduid/compose.yml ]; then
     echo "Run $0 from the se-leg-developer top level directory"
     exit 1
 fi
 
+#
+# Set up entrys in /etc/hosts for the containers with externally accessible services
+#
+(printf "172.16.20.100\tse-leg-op.seleg_dev se-leg-op\n";
+) \
+    | while read line; do
+    if ! grep -q "^${line}$" /etc/hosts; then
+	echo "$0: Adding line '${line}' to /etc/hosts"
+	if [ "x`whoami`" = "xroot" ]; then
+	    echo "${line}" >> /etc/hosts
+	else
+	    echo "${line}" | sudo tee -a /etc/hosts
+	fi
+    else
+	echo "Line '${line}' already in /etc/hosts"
+    fi
+done
+
+./bin/docker-compose -f eduid/compose.yml rm -f --all
 ./bin/docker-compose -f se-leg/compose.yml up $*
