427602ad19e9a190a659fa111cc18d91761e4607
Johan Lundberg <lundberg@nordu.net>
2018-05-12 17:08:50 +0200

    MONGO_URI -> DB_URI

--- se-leg-ra/etc/app_config.py
+++ se-leg-ra/etc/app_config.py
@@ -1,6 +1,6 @@
 DEBUG = True
 SECRET_KEY = 'notasecret'
-MONGO_URI = 'mongodb://se_leg_ra:se_leg_ra_pw@mongodb'
+DB_URI = 'mongodb://se_leg_ra:se_leg_ra_pw@mongodb'
 LOGOUT_URL = 'https://rasp.se-leg.docker/Shibboleth.sso/Logout'
 RA_APP_ID = 'RA-APP-1'
 RA_APP_SECRET = 'secret'
